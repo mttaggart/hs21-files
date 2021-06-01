e20ae96441347a4e9641554f19d865749378df71
Michael Taggart <michael.t.taggart@gmail.com>
2021-05-31 22:56:34 -0700

    Add quizengine

---
+++ quizengine/quizengine/quizengine.py
@@ -0,0 +1,46 @@
+import json
+import sys
+
+class Quiz:
+    def __init__(self, json_file):
+        with open(json_file) as f:
+            data = json.load(f)
+            self.questions = [Question(q) for q in data["questions"]]
+            self.flag = data["flag"]
+
+    def run(self):
+        results = []
+        for q in self.questions:
+            result = q.ask()
+            if result:
+                print("Nice work!")
+            else:
+                print(q.feedback)
+            results.append(result)
+        if all(results):
+            print("Congratulations! Here's your flag:")
+            print(f"{self.flag['user']}:{self.flag['flag']}")
+        else:
+            correct = len(list(filter(lambda r: r, results)))
+            print(f"{correct}/{len(results)} correct. Review the feedback above and try again!")
+
+class Question:
+    def __init__(self, data):
+        self.question = data["question"]
+        self.answer = data["answer"]
+        self.feedback = data["feedback"]
+
+    def ask(self):
+        response = input(f"{self.question} ")
+        return self.answer.lower() in response.lower()
+
+def main():
+    try:
+        quiz_file = sys.argv[1]
+    except:
+        print("Could not load quiz file")
+    quiz = Quiz(quiz_file)
+    quiz.run()
+
+if __name__ == "__main__":
+    main()
