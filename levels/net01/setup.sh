8bee7fd50219691e28e9e34d0f060c26e0851bd1
Michael Taggart <michael.t.taggart@gmail.com>
2021-06-09 06:29:20 -0700

    Add net01 setup

---
+++ levels/net01/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+QUIZ=/home/hs21/hs21-files/levels/net01/quiz.json
+
+IP=`ip a s enp0s3 | grep "inet 10" | tr -s " " | cut -d " " -f 3 | sed "s/\/.\+//g"`
+
+GATEWAY=`ip r | grep default | cut -d " " -f 3`
+
+sed -ie "s/<<IP>>/$IP/g" quiz.json
+
+sed -ie "s/<<GATEWAY>>/$GATEWAY/g" quiz.json
