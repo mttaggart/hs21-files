f8ef0cb1808723681b853c9c5bb5efa999a58728
Michael Taggart <michael.t.taggart@gmail.com>
2021-06-11 23:55:49 -0700

    Fix pathing

--- levels/cmd02/setup.sh
+++ levels/cmd02/setup.sh
@@ -4,14 +4,15 @@
 uid=`id | cut -d " " -f 1 | sed -e "s/uid=[0-9]\+(\(.\+\))/\1/g"`
 
 # Fill in uid
-sed -ie "s/<<UID>>/$uid/g" /home/hs21/hs21-files/quiz.json
+sed -ie "s/<<UID>>/$uid/g" /home/hs21/hs21-files/levels/cmd02/quiz.json
 
 # Fix sudoers
 echo "cmd02 ALL=(ALL:ALL) /home/cmd02/runasroot.sh" >> /etc/sudoers
 
 # Fix permissions
-chown root:root runasroot.sh
-chmod 711 runasroot.sh
-chmod +s runasroot.sh
+RUNASROOT=/home/cmd02/runasroot.sh
+chown root:root $RUNASROOT
+chmod 711 $RUNASROOT
+chmod +s $RUNASROOT
 
 
