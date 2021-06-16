4ec29035d36d2951b56e535e8667996908f9b5a3
Michael Taggart <michael.t.taggart@gmail.com>
2021-06-15 22:50:19 -0700

    Add runme setup

--- levels/cmd03/setup.sh
+++ levels/cmd03/setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+QUIZ_FILE=/home/$SUDO_USER/hs21-files/levels/cmd03/quiz.json
+
+# Get flags
+FLAG=HS21{`curl --no-progress-meter https://passphrase.taggart-tech.com/api/pw`}
+GREPFLAG=HS21{`curl --no-progress-meter https://passphrase.taggart-tech.com/api/pw`}
+FINDFLAG=HS21{`curl --no-progress-meter https://passphrase.taggart-tech.com/api/pw`}
+
+# Setup find
+mkdir -p /tmp/hs21/cmd03
+echo $FLAG > /tmp/hs21/cmd03/cmd03flag.txt
+
+# Setup runme
+sed -ie "s/<<QUIZ_FILE>>/$QUIZ_FILE/g" /home/cmd03/runme
+sed -ie "s/<<FLAG>>/$FLAG/g" $QUIZ_FILE
+sed -ie "s/<<GREPFLAG>>/$GREPFLAG/g" $QUIZ_FILE
+sed -ie "s/<<FINDFLAG>>/$FINDFLAG/g" $QUIZ_FILE
+
 chmod 711 /home/cmd03/runme
-chmod +s /home/cmd03/runme
