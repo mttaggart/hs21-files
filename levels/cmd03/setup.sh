59340be5fef0c14236077e2518d6ccc16a188f51
Michael Taggart <michael.t.taggart@gmail.com>
2021-06-16 22:50:24 -0700

    Fix runme permissions

--- levels/cmd03/setup.sh
+++ levels/cmd03/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 QUIZ_FILE=/home/$SUDO_USER/hs21-files/levels/cmd03/quiz.json
+RUNME=/home/cmd03/runme
 
 # Get flags
 FLAG=HS21{`curl --no-progress-meter https://passphrase.taggart-tech.com/api/pw`}
@@ -15,11 +16,14 @@ echo $FINDFLAG > /tmp/hs21/cmd03/cmd03flag.txt
 echo "Setting up runme"
 sed -i -e "s@<<QUIZ_FILE>>@$QUIZ_FILE@g" /home/cmd03/runme
 
-
 # Setup quiz file
 echo "Setting up quiz file"
 sed -i -e "s/<<FLAG>>/$FLAG/g" $QUIZ_FILE
 sed -i -e "s/<<GREPFLAG>>/$GREPFLAG/g" $QUIZ_FILE
 sed -i -e "s/<<FINDFLAG>>/$FINDFLAG/g" $QUIZ_FILE
 
-chmod 711 /home/cmd03/runme
+# Change runme perms
+chown root:root $RUNME
+chmod 711 $RUNME
+chmod +s $RUNME
+
