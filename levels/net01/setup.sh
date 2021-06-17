b2e8ddf1dbe18cdaf42c40ac559b378e242f18a5
Michael Taggart <michael.t.taggart@gmail.com>
2021-06-16 23:39:12 -0700

    net01 setup

--- levels/net01/setup.sh
+++ levels/net01/setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 QUIZ=/home/hs21/hs21-files/levels/net01/quiz.json
-
 IP=`ip a s enp0s3 | grep "inet 10" | tr -s " " | cut -d " " -f 3 | sed "s/\/.\+//g"`
-
 GATEWAY=`ip r | grep default | cut -d " " -f 3`
+FLAG=HS21{`curl --no-progress-meter https://passphrase.taggart-tech.com/api/pw`}
 
 sed -ie "s/<<IP>>/$IP/g" $QUIZ
-
 sed -ie "s/<<GATEWAY>>/$GATEWAY/g" $QUIZ
+sed -ie "s/<<FLAG>>/$FLAG/g" $QUIZ
