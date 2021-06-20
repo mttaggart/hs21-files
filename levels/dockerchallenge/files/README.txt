57ff73d4f1bff23e91240677f152c3248b21902b
Michael Taggart <michael.t.taggart@gmail.com>
2021-06-20 15:46:22 -0700

    Docker challenge

---
+++ levels/dockerchallenge/files/README.txt
@@ -0,0 +1,2 @@
+A Docker image is available on this machine. Run a shell in it in interactive mode, and seek the flag in common locations.
+
---
+++ levels/dockerchallenge/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+HOMEDIR=/home/dockerchallenge
+LEVELDIR=/home/$SUDO_USER/hs21-files/levels/dockerchallenge
+
+# Remove extraneous quiz
+rm $HOMEDIR/quiz
+
+# Add user to docker group
+gpasswd -a dockerchallenge docker
+
+NEXT_USER=`cat $LEVELDIR/quiz.json | jq -r .flag.user`
+FLAG=`cat $LEVELDIR/quiz.json | jq -r .flag.flag`
+
+sed -ie "s/<<NEXT_USER>>/$NEXT_USER/g" $LEVELDIR/Dockerfile
+sed -ie "s/<<FLAG>>/$FLAG/g" $LEVELDIR/Dockerfile
+
+# Build Docker image
+docker image build --quiet -t "dockerchallenge:latest" $LEVELDIR/
